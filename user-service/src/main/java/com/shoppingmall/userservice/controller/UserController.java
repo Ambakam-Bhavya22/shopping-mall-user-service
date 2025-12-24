da5763aa5260bb7149de6959764f7fe064f29215
Ambakam-Bhavya22 <bhavyaprasanth22@gmail.com>
2025-12-24 18:26:57 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/shoppingmall/userservice/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.shoppingmall.userservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.shoppingmall.userservice.entity.User;
+import com.shoppingmall.userservice.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
